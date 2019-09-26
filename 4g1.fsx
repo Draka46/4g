db1b84115a1d91d8f99037f27bdf814934a7c944
Draka46 <Draka46@hotmail.com>
2019-09-26 10:42:31 +0200

    Test

--- 4g1.fsx
+++ 4g1.fsx
@@ -4,20 +4,22 @@ open System
 
 printfn "White-box testing of vex2d.fsx"
 printfn "Unit : len"
-printfn "Test 1 : %b | len (4,0) = 4" (vec2d.len (4.0, 0.0) = 4.0)
-printfn "Test 2 : %b | len (0,6) = 6" (vec2d.len (0.0, 6.0) = 6.0)
-printfn "Test 3 : %b | len (sqrt(10), sqrt(15)) = 5" (vec2d.len (sqrt 10.0, sqrt 15.0) = 5.0)
-printfn "Test 4 : %b | len (sqrt(5), sqrt(11)) = 4" (vec2d.len (sqrt 5.0, sqrt 11.0) = 4.0)
+printfn "Test 1 : %5b | len (4,0) = 4" (vec2d.len (4.0, 0.0) = 4.0)
+printfn "Test 2 : %5b | len (0,6) = 6" (vec2d.len (0.0, 6.0) = 6.0)
+printfn "Test 3 : %5b | len (sqrt(10), sqrt(15)) = 5" (vec2d.len (sqrt 10.0, sqrt 15.0) = 5.0)
+printfn "Test 4 : %5b | len (sqrt(5), sqrt(11)) = 4" (vec2d.len (sqrt 5.0, sqrt 11.0) = 4.0)
 
 /// My inverse tangent tool (Microsoft Mathematics) keeps disagreeing with the program about results of tan^(-1)
 printfn "Unit : ang"
-printfn "Test 1 : %b | ang (4, pi) = pi/4 | ang (4, pi): %A | pi/4: %A" (vec2d.ang (4.0, Math.PI) = (Math.PI/4.0)) (vec2d.ang (4.0, Math.PI)) (Math.PI/4.0)
+printfn "Test 1 : %5b | ang (4, pi) = pi/4 | ang (4, pi): %A | pi/4: %A" (vec2d.ang (4.0, Math.PI) = (Math.PI/4.0)) (vec2d.ang (4.0, Math.PI)) (Math.PI/4.0)
 
 printfn "Unit : scale"
-printfn "Test 1 : %b | scale 2 (4,0) = (8,0)" (vec2d.scale 2.0 (4.0, 0.0) = (8.0, 0.0))
+printfn "Test 1 : %5b | scale 2 (4,0) = (8,0)" (vec2d.scale 2.0 (4.0, 0.0) = (8.0, 0.0))
 
 printfn "Unit : add"
-printfn "Test 1 : %b | add (4,0) (2,0) = (6,0)" (vec2d.add (4.0, 0.0) (2.0, 0.0) = (6.0, 0.0))
+printfn "Test 1 : %5b | add (4,0) (2,0) = (6,0)" (vec2d.add (4.0, 0.0) (2.0, 0.0) = (6.0, 0.0))
 
 printfn "Unit : dot"
-printfn "Test 1 : %b | dot (4,0) (2,0) = (6,0)" (vec2d.dot (4.0, 0.0) (2.0, 0.0) = 8.0)
+printfn "Test 1 : %5b | dot (4,0) (2,0) = (6,0)" (vec2d.dot (4.0, 0.0) (2.0, 0.0) = 8.0)
+
+// printfn "  %5b: Branch 1a" (...)
---
+++ tempTable.fsx
@@ -0,0 +1,25 @@
+﻿printf "Enter n (table n) printing: "
+let input = float (System.Console.ReadLine())
+let n = (int) input
+
+let str_len k : int = k.ToString().Length
+let prnt_spaces max k : string = let mutable spac = "" in for j = 1 to (max - str_len(k)) do spac <- spac + " " done; spac
+
+let table n (len_array : _ array) circ_omk : string =
+    let spaces_max = if (str_len(circ_omk) > str_len("cirkel omkreds")) then str_len(circ_omk) else str_len("cirkel omkreds")
+    
+    let mutable s = "  "
+    do s <- s + prnt_spaces spaces_max "n" + "n"
+    do s <- s + prnt_spaces spaces_max "længde" + "længde"
+    do s <- s + prnt_spaces spaces_max "cirkel omkreds" + "cirkel omkreds\n"
+    
+    for i = 1 to n do
+        s <- s + "  "
+        for j = 1 to 3 do
+        let len = len_array.[i].ToString()
+        let omkS = circ_omk.ToString()
+        s <- s + (prnt_spaces spaces_max i) + i.ToString() + (prnt_spaces spaces_max len) + len + (prnt_spaces spaces_max omkS) + omkS
+
+    s
+
+printfn "%s" (table n [| 1,2,3,4,5,7,8,9,10,100,1000 |] 1)
